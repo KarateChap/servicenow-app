92968d8e5d34f8c162df6e1094866820aedcd776
Karte Chap <57673613+KarateChap@users.noreply.github.com>
2023-04-03 02:32:18 +0800

    Delete environment.ts

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  firebase: {
-    projectId: 'work-apps-c2809',
-    appId: '1:533241734286:web:71b5e8c418edd5757cd5df',
-    storageBucket: 'work-apps-c2809.appspot.com',
-    apiKey: 'AIzaSyClP3O7G2MFnH7_-SqzKSlsUQwmwqf4-hE',
-    authDomain: 'work-apps-c2809.firebaseapp.com',
-    messagingSenderId: '533241734286',
-  },}
